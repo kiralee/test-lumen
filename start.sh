82b29597f35907908b148155d96bc17cd1d914ce
Tam Le <leminhtamboy@gmail.com>
2021-06-20 16:02:32 +0700

    Test jenkins

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker-compose up -d
-echo "###### Running composer install"
+echo "####### Running composer install"
 docker exec container_php_fpm composer install
-echo "###### Copy env file"
+echo "####### Copy env file"
 docker exec container_php_fpm composer run post-root-package-install
-echo "###### Start Queue and keep idle"
+echo "####### Start Queue and keep idle"
 docker exec container_php_fpm php artisan queue:listen
